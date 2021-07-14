753d552d598610af730835b71d03607ea90e246a
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-07-14 08:55:29 -0700

    GNG-438: Add api/users/update route to back-end

--- routes/user/index.js
+++ routes/user/index.js
@@ -58,4 +58,48 @@ router.post("/create", (req, res) => {
     });
 });
 
+router.post("/update", (req, res) => {
+  const username = req.body.username || "";
+  const newPassword = req.body.password;
+
+  // Check if username exists in database
+  knex()("users")
+    .select("*")
+    .where("username", username)
+    .then((rows) => {
+      // If username isn't in users table, 4xx response
+      if (rows?.length === 0) {
+        console.log(`/update: No username ${username} found in database`);
+
+        res.status(400).json({
+          updated: false,
+        });
+      } else if (rows?.length === 1 && rows[0]?.username === username) {
+        console.log(`/update: Username ${username} found in database`);
+
+        bcrypt.hash(newPassword, saltRounds).then((hash) => {
+          knex()("users")
+            .where({ username: username })
+            .update({ hash: hash })
+            .then((success) => {
+              console.log(
+                `/update: Updated username in users table: ${username}`
+              );
+
+              res.status(200).json({
+                created: true,
+                username: username,
+              });
+            })
+            .catch((err) => {
+              console.error(
+                `/update: Error generating bcrypt hash for user ${username}:`
+              );
+              console.error(err.message);
+            });
+        });
+      }
+    });
+});
+
 module.exports = router;
