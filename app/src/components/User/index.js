c48c790e1d13ccaac21de4dc2353a80b688eb826
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-07-20 16:21:55 -0700

    GNG-438: Add Logout and AllUsers components to Users panel

--- app/src/components/User/index.js
+++ app/src/components/User/index.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 import AddUser from "./AddUser";
 import UpdateUser from "./UpdateUser";
+import Logout from "./Logout";
+import AllUsers from "./AllUsers";
 
 const StyledDiv = styled.div`
   background-color: white;
@@ -15,11 +17,23 @@ function UserRoutes() {
       <h2>User Routes</h2>
       <div>
         <h3>Add user</h3>
+        <span>POST /api/users/add</span>
         <AddUser />
       </div>
       <div>
         <h3>Update user</h3>
+        <span>POST /api/users/update</span>
         <UpdateUser />
+      </div>
+      <div>
+        <h3>Logout</h3>
+        <span>POST /api/users/logout</span>
+        <Logout />
+      </div>
+      <div>
+        <h3>All Users</h3>
+        <span>GET /api/users/all</span>
+        <AllUsers />
       </div>
     </StyledDiv>
   );
