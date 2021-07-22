b3f2307ce61c6a62f45257fff79677b36f5fbf14
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-07-22 14:43:34 -0700

    GNG-443: Update App route to Editor to pass id parameter

--- app/src/App.js
+++ app/src/App.js
@@ -22,7 +22,7 @@ function App() {
           <Route path="/login">
             <Login />
           </Route>
-          <PrivateRoute path="/editor">
+          <PrivateRoute path="/edit/:id">
             <Editor />
           </PrivateRoute>
           <PrivateRoute path="/">
