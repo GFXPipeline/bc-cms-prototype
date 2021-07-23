0ea776333e972b74815d8e2a40949e441ba10aad
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-07-23 15:52:17 -0700

    GNG-443: Add create route in front-end page service

--- app/src/_services/page.service.js
+++ app/src/_services/page.service.js
@@ -3,6 +3,31 @@ import axios from "axios";
 import { authHeader } from "../_helpers";
 import { authenticationService } from "../_services";
 
+// POST request to /api/page
+async function create({ data, title }) {
+  try {
+    const headers = authHeader();
+
+    const newPageData = {
+      username: authenticationService.currentUserValue.username,
+      title: title,
+      data: data,
+    }
+
+    const response = await axios({
+      method: "POST",
+      url: "/api/page",
+      headers,
+      data: newPageData,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log("Error in page.service create: ", error);
+    throw error;
+  }
+}
+
 // GET request to /api/page/:id
 async function read(id) {
   console.log("Inside pageService.read, id: ", id);
@@ -11,12 +36,11 @@ async function read(id) {
       headers: authHeader(),
       username: authenticationService.currentUserValue.username,
     };
-    console.log("requestOptions: ", requestOptions);
     const response = await axios.get(`/api/page/${id}`, requestOptions);
 
     console.log("Response in page.service read: ", response);
     if (response?.data && Array.isArray(response?.data)) {
-      return response?.data?.[0]?.data;
+      return response?.data?.[0];
     } else {
       const error = "Response in page.service.read is not an array";
       throw error;
@@ -28,5 +52,6 @@ async function read(id) {
 }
 
 export const pageService = {
+  create,
   read,
 };
