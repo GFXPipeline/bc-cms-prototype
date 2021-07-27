8428abbc9d21c598cc4ed9d85ba108b130e32dc9
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-07-27 16:47:10 -0700

    GNG-443: Add back-end GET /api/user/:username route

--- server.js
+++ server.js
@@ -230,6 +230,40 @@ apiRouter.get("/pages/all", (req, res) => {
     });
 });
 
+apiRouter.get("/user/:username", (req, res) => {
+  console.log(`GET /api/user/${req?.params?.username}`);
+
+  // If username was supplied, look it up in database
+  if (req?.params?.username) {
+    knex("users")
+      .select([
+        "id",
+        "username",
+        "is_admin",
+        "time_created",
+        "time_last_updated",
+      ])
+      .where("username", req?.params?.username)
+      .then((rows) => {
+        if (rows?.length > 0) {
+          res.status(200).send(rows[0]);
+        } else {
+          res.status(404).send("404");
+        }
+      })
+      .catch((error) => {
+        console.log(
+          `error in GET /api/user/${req?.params?.username} knex call: `,
+          error
+        );
+        res.status(500).send("500");
+      });
+  } else {
+    // No username parameter provided
+    res.status(404).send("404");
+  }
+});
+
 apiRouter.get("/users/all", (req, res) => {
   console.log("GET /api/users/all");
 
