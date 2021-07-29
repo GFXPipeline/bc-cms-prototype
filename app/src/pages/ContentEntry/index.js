78d55f190c33244fa03608ea8244a649af2e2650
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-07-29 12:12:12 -0700

    GNG-466: Add page control buttons to ContentEntry page

--- app/src/pages/ContentEntry/index.js
+++ app/src/pages/ContentEntry/index.js
@@ -7,12 +7,22 @@ import TextInput from "../../components/TextInput";
 
 const ContentContainer = styled.div`
   background-color: white;
-  max-width: 800px;
-  width: 60%;
+  width: 100%;
+`;
 
-  label {
-    font-size: 13px;
-    font-weight: 700;
+const LeftPanel = styled.div`
+  align-items: stretch;
+  display: flex;
+  flex-direction: column;
+  max-width: 368px;
+
+  div.top {
+    padding: 13px;
+
+    label {
+      font-size: 13px;
+      font-weight: 700;
+    }
   }
 `;
 
@@ -20,7 +30,28 @@ const InputContainer = styled.div`
   align-items: center;
   display: flex;
   flex-direction: row;
+  margin: 8px 0;
   max-height: 44px;
+`;
+
+const PageControlToolbar = styled.div`
+  border-top: 1px solid #707070;
+  border-bottom: 1px solid #707070;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+    font-size: 14px;
+    height: 44px;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 function ContentEntry() {
@@ -28,20 +59,33 @@ function ContentEntry() {
     <>
       <Header />
       <ContentContainer>
-        <label htmlFor="content-search">Search by page name, ID, or URL</label>
-        <InputContainer>
-          <TextInput id="content-search" />
-          <Button primary>Search</Button>
-        </InputContainer>
-
-        <label htmlFor="content-list-view">List view</label>
-        <InputContainer>
-          <Select
-            id="content-list-view"
-            name="content-list-view"
-            options={[{ id: "view-all", label: "View all" }]}
-          />
-        </InputContainer>
+        <LeftPanel>
+          <div className="top">
+            <label htmlFor="content-search">
+              Search by page name, ID, or URL
+            </label>
+            <InputContainer>
+              <TextInput id="content-search" />
+              <Button primary>Search</Button>
+            </InputContainer>
+            <label htmlFor="content-list-view">List view</label>
+            <InputContainer>
+              <Select
+                id="content-list-view"
+                name="content-list-view"
+                options={[{ id: "view-all", label: "View all" }]}
+              />
+            </InputContainer>
+          </div>
+          <PageControlToolbar>
+            <button>Create</button>
+            <button>Lock</button>
+            <button>Move</button>
+            <button>Publish</button>
+            <button>Tag</button>
+            <button>Delete</button>
+          </PageControlToolbar>
+        </LeftPanel>
       </ContentContainer>
     </>
   );
