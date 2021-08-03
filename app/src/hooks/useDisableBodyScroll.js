4641a962d62e6529782ffd55c55fac83fb6f1f26
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-08-03 16:31:35 -0700

    GNG-466: Add useDisableBodyScroll custom hook

---
+++ app/src/hooks/useDisableBodyScroll.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+// Prevent the body of the page from scrolling, such as when a modal is open
+export const useDisableBodyScroll = ({ isScrollDisabled }) => {
+  useEffect(() => {
+    if (isScrollDisabled) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  }, [isScrollDisabled]);
+};
