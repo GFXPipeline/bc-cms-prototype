aa0939f132771c6f2c9685a38f90f6ab674b09a7
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-08-23 09:17:15 -0700

    GNG-479: Update ContentEntry modal tooltip styling to show outer arrow

--- app/src/pages/ContentEntry/_actions/CreatePage/PickTemplateInfoBox.js
+++ app/src/pages/ContentEntry/_actions/CreatePage/PickTemplateInfoBox.js
@@ -2,31 +2,41 @@ import styled from "styled-components";
 
 const StyledDiv = styled.div`
   background-color: white;
+  font-size: 16px;
+
   max-width: 465px;
   opacity: 1 !important;
+
+  div {
+    max-height: 340px;
+    overflow-y: auto;
+    padding: 8px 21px;
+  }
 `;
 
 function PickTemplateInfoBox() {
   return (
     <StyledDiv>
-      <h2>Page templates</h2>
-      <p>
-        To streamline content creation, input your content in designated page
-        templates and avoid having to format the page yourself. See Page
-        Templates in the Guide.
-      </p>
-      <strong>Base Template</strong>
-      <p>The generic page template used for most government topic pages</p>
-      <strong>Service Template</strong>
-      <p>
-        Used to build out a step-by-step or transactional service or program
-      </p>
-      <strong>Theme page template</strong>
-      <p>Used to build a theme page</p>
-      <strong>Search Results Page Template</strong>
-      <p>Used to link a search to a results page</p>
-      <strong>Forms Template</strong>
-      <p>Used to create a form</p>
+      <div>
+        <h2>Page templates</h2>
+        <p>
+          To streamline content creation, input your content in designated page
+          templates and avoid having to format the page yourself. See Page
+          Templates in the Guide.
+        </p>
+        <strong>Base Template</strong>
+        <p>The generic page template used for most government topic pages</p>
+        <strong>Service Template</strong>
+        <p>
+          Used to build out a step-by-step or transactional service or program
+        </p>
+        <strong>Theme page template</strong>
+        <p>Used to build a theme page</p>
+        <strong>Search Results Page Template</strong>
+        <p>Used to link a search to a results page</p>
+        <strong>Forms Template</strong>
+        <p>Used to create a form</p>
+      </div>
     </StyledDiv>
   );
 }
--- app/src/pages/ContentEntry/_actions/CreatePage/SelectPageInfoBox.js
+++ app/src/pages/ContentEntry/_actions/CreatePage/SelectPageInfoBox.js
@@ -2,15 +2,24 @@ import styled from "styled-components";
 
 const StyledDiv = styled.div`
   background-color: white;
+  font-size: 16px;
   max-width: 465px;
   opacity: 1 !important;
+
+  div {
+    max-height: 340px;
+    overflow-y: auto;
+    padding: 8px 21px;
+  }
 `;
 
 function SelectPageInfoBox() {
   return (
     <StyledDiv>
-      <h2>Page types</h2>
-      <strong>Topic Page</strong>
+      <div>
+        <h2>Page types</h2>
+        <strong>Topic Page</strong>
+      </div>
     </StyledDiv>
   );
 }
