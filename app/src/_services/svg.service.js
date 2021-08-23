8370c23051b278f91ae60bda81eeb1e8eac9ab30
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-08-23 16:50:54 -0700

    GNG-480: Add calendar SVG icon to svgService

--- app/src/_services/svg.service.js
+++ app/src/_services/svg.service.js
@@ -1,8 +1,10 @@
 // SVG assets imported as React components
+import { ReactComponent as calendar } from "../assets/noun-calendar.svg";
 import { ReactComponent as info } from "../assets/noun-info.svg";
 
 // Asset map
 const svgIcons = {
+  "noun-calendar.svg": calendar,
   "noun-info.svg": info,
 };
 
