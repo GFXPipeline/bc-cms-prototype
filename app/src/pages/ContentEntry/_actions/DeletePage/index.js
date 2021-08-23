c0e8f4e0fc7b01eac50c36f8dc351d9723ef5cbc
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-08-23 16:52:20 -0700

    GNG-480: Add time and set selectors to ContentEntry/DeletePage modal

--- app/src/pages/ContentEntry/_actions/DeletePage/index.js
+++ app/src/pages/ContentEntry/_actions/DeletePage/index.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
+import DatePicker from "react-datepicker";
 import styled from "styled-components";
 
+import "react-datepicker/dist/react-datepicker.css";
+
 // App-level components
+import svgCalendar from "../../../../assets/noun-calendar.svg";
+import Icon from "../../../../components/Icon";
 import Modal from "../../../../components/Modal";
 import Button from "../../../../components/Button";
 
@@ -84,6 +89,111 @@ const StyledModal = styled(Modal)`
         }
       }
 
+      div.set-time-and-date {
+        align-items: center;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin: 0 0 12px 0;
+
+        div.date-and-time {
+          display: flex;
+          flex-direction: row;
+
+          span {
+            font-size: 13px;
+          }
+
+          div.date-select {
+            display: flex;
+            flex-direction: column;
+            margin-right: 17px;
+
+            div.react-datepicker-wrapper {
+              border: 2px solid #3e3e3e;
+              height: 48px;
+              width: 144px;
+
+              div.react-datepicker__input-container {
+                input[type="text"] {
+                  background-image: url(${svgCalendar});
+                  background-position: right 16px top 50%;
+                  background-repeat: no-repeat;
+                  background-size: 20px 20px;
+                  border: none;
+                  font-family: "BCSans", "Noto Sans", Verdana, Arial, sans-serif;
+                  font-size: 16px;
+                  height: 44px;
+                  padding-left: 13px;
+                  width: 140px;
+                }
+              }
+            }
+          }
+
+          div.time-select {
+            display: flex;
+            flex-direction: column;
+
+            div.input-container {
+              align-items: end;
+              display: flex;
+              flex-direction: row;
+              height: 48px;
+
+              input[type="number"] {
+                border: 2px solid #3e3e3e;
+                height: 48px;
+              }
+
+              span.colon {
+                font-size: 16px;
+                font-weight: 700;
+                margin: 0 4px;
+              }
+
+              fieldset.radio-period {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                height: 48px;
+                margin-left: 6px;
+
+                label {
+                  border: 2px solid #3e3e3e;
+                  display: flex;
+                  flex-direction: column;
+                  align-items: center;
+                  height: 24px;
+                  width: 35px;
+
+                  &.selected {
+                    background-color: #3e3e3e;
+                    color: white;
+                  }
+
+                  &:focus-within {
+                    outline: 3px solid royalblue;
+                  }
+
+                  input[type="radio"] {
+                    height: 0px;
+                    opacity: 0.01;
+                    width: 0px;
+                  }
+
+                  span {
+                    font-size: 16px;
+                    font-weight: 400;
+                    margin-top: -3px;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+
       div.control-buttons {
         display: flex;
         flex-direction: row;
@@ -112,6 +222,10 @@ const StyledModal = styled(Modal)`
 function DeletePage({ id, isOpen, setIsOpen }) {
   const [deleteType, setDeleteType] = useState("soft-delete");
   const [reason, setReason] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [timeHour, setTimeHour] = useState(parseInt(12));
+  const [timeMinute, setTimeMinute] = useState(parseInt(0));
+  const [period, setPeriod] = useState("am");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
   const [isError, setIsError] = useState(false);
@@ -185,6 +299,62 @@ function DeletePage({ id, isOpen, setIsOpen }) {
             rows={3}
             requried
           />
+        </div>
+        <div className="set-time-and-date">
+          <label>
+            <input type="checkbox" />
+            <span>Set time and date</span>
+          </label>
+          <div className="date-and-time">
+            <div className="date-select">
+              <span>Date</span>
+              <DatePicker selected={date} onChange={(date) => setDate(date)} />
+            </div>
+            <div className="time-select">
+              <span>Time</span>
+              <div className="input-container">
+                <input
+                  type="number"
+                  id="hour"
+                  min="1"
+                  max="12"
+                  value={timeHour}
+                  onChange={(e) => setTimeHour(parseInt(e.target.value))}
+                />
+                <span className="colon">:</span>
+                <input
+                  type="number"
+                  id="minute"
+                  min="0"
+                  max="59"
+                  value={timeMinute}
+                  onChange={(e) => setTimeMinute(parseInt(e.target.value))}
+                />
+                <fieldset className="radio-period">
+                  <label className={period === "am" ? "selected" : null}>
+                    <input
+                      type="radio"
+                      name="period"
+                      value="am"
+                      checked={period === "am"}
+                      onChange={(e) => setPeriod("am")}
+                    />
+                    <span>AM</span>
+                  </label>
+                  <label className={period === "pm" ? "selected" : null}>
+                    <input
+                      type="radio"
+                      name="period"
+                      value="pm"
+                      checked={period === "pm"}
+                      onChange={(e) => setPeriod("pm")}
+                    />
+                    <span>PM</span>
+                  </label>
+                </fieldset>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="control-buttons">
           <Button
