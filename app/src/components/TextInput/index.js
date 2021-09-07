38f3b18d702a4cd230e760e90a0f7bb4ab5c75d7
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-09-07 14:01:59 -0700

    GNG-482: Add default font-size 16px to TextInput component

--- app/src/components/TextInput/index.js
+++ app/src/components/TextInput/index.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const StyledTextInput = styled.input`
   border-radius: 0px;
   border: 2px solid #3e3e3e;
+  font-size: 16px;
   height: 44px;
   padding: 4px;
   width: 100%;
