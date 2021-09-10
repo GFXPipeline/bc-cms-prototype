cc82b0822a54a37e3318d1a18730d26a37840871
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-09-10 11:07:36 -0700

    GNG-483: Add undo and redo icons to SVG service

--- app/src/_services/svg.service.js
+++ app/src/_services/svg.service.js
@@ -1,11 +1,15 @@
 // SVG assets imported as React components
 import { ReactComponent as calendar } from "../assets/noun-calendar.svg";
 import { ReactComponent as info } from "../assets/noun-info.svg";
+import { ReactComponent as undo } from "../assets/fa-undo-solid.svg";
+import { ReactComponent as redo } from "../assets/fa-redo-solid.svg";
 
 // Asset map
 const svgIcons = {
   "noun-calendar.svg": calendar,
   "noun-info.svg": info,
+  "fa-undo-solid.svg": undo,
+  "fa-redo-solid.svg": redo,
 };
 
 function getSvg(id) {
