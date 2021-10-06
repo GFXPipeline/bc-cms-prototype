ddeffa71835849147cf95017aeb9cc41586b1996
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-10-06 14:10:06 -0700

    GNG-492: Add plus sign icon to SVG service

--- app/src/_services/svg.service.js
+++ app/src/_services/svg.service.js
@@ -11,6 +11,7 @@ import { ReactComponent as eye } from "../assets/fa-eye.svg";
 import { ReactComponent as link } from "../assets/fa-link.svg";
 import { ReactComponent as list } from "../assets/fa-list.svg";
 import { ReactComponent as lock } from "../assets/fa-lock.svg";
+import { ReactComponent as plus } from "../assets/fa-plus.svg";
 import { ReactComponent as trash } from "../assets/fa-trash.svg";
 import { ReactComponent as undo } from "../assets/fa-undo-solid.svg";
 import { ReactComponent as redo } from "../assets/fa-redo-solid.svg";
@@ -23,6 +24,7 @@ const svgIcons = {
   "fa-link.svg": link,
   "fa-list.svg": list,
   "fa-lock.svg": lock,
+  "fa-plus.svg": plus,
   "fa-trash.svg": trash,
   "fa-undo-solid.svg": undo,
   "fa-redo-solid.svg": redo,
