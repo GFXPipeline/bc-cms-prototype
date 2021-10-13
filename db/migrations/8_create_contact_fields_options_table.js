054da038684b7103bf4f236d1381cfae9bfbf74d
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-10-13 14:31:16 -0700

    GNG-492: Add contact_field_types and contact_field_options tables

---
+++ db/migrations/8_create_contact_fields_options_table.js
@@ -0,0 +1,28 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('contact_field_options', (table) => {
+    table
+      .uuid('id')
+      .defaultTo(knex.raw(`gen_random_uuid()`)) // Postgres built-in UUID v4 generator
+      .notNullable()
+      .primary();
+    table
+      .uuid('type_id')
+      .notNullable()
+      .references('id')
+      .inTable('contact_field_types');
+    table.string('name').unique().notNullable();
+    table.string('display_name').notNullable();
+    table.boolean('is_marked_for_deletion').defaultTo(false);
+    table.uuid('created_by_user').references('id').inTable('users');
+    table.uuid('owned_by_user').references('id').inTable('users');
+    table.uuid('last_modified_by_user').references('id').inTable('users');
+    table.uuid('marked_for_deletion_by_user').references('id').inTable('users');
+    table.dateTime('time_created').notNullable().defaultTo(knex.fn.now());
+    table.dateTime('time_last_updated').notNullable().defaultTo(knex.fn.now());
+    table.dateTime('time_marked_for_deletion');
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('contact_field_options');
+};
