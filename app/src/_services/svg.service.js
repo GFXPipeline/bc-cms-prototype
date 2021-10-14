58ba43cf7efa98b5cebe4f1b9959cf5bae8746cc
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-10-13 18:40:27 -0700

    GNG-492: Add envelope and map marker icons to SVG service

--- app/src/_services/svg.service.js
+++ app/src/_services/svg.service.js
@@ -4,6 +4,7 @@ import { ReactComponent as arrowDown } from "../assets/ionic-ios-arrow-down.svg"
 import { ReactComponent as arrowForward } from "../assets/ionic-ios-arrow-forward.svg";
 import { ReactComponent as arrowUp } from "../assets/ionic-ios-arrow-up.svg";
 import { ReactComponent as calendar } from "../assets/noun-calendar.svg";
+import { ReactComponent as envelope } from "../assets/fa-envelope.svg";
 import { ReactComponent as phone } from "../assets/fa-phone.svg";
 import { ReactComponent as info } from "../assets/noun-info.svg";
 import { ReactComponent as copy } from "../assets/fa-copy.svg";
@@ -11,6 +12,7 @@ import { ReactComponent as eye } from "../assets/fa-eye.svg";
 import { ReactComponent as link } from "../assets/fa-link.svg";
 import { ReactComponent as list } from "../assets/fa-list.svg";
 import { ReactComponent as lock } from "../assets/fa-lock.svg";
+import { ReactComponent as mapMarker } from "../assets/fa-map-marker.svg";
 import { ReactComponent as plus } from "../assets/fa-plus.svg";
 import { ReactComponent as trash } from "../assets/fa-trash.svg";
 import { ReactComponent as undo } from "../assets/fa-undo-solid.svg";
@@ -19,11 +21,13 @@ import { ReactComponent as redo } from "../assets/fa-redo-solid.svg";
 // Asset map
 const svgIcons = {
   "fa-copy.svg": copy,
+  "fa-envelope.svg": envelope,
   "fa-eye.svg": eye,
   "fa-phone.svg": phone,
   "fa-link.svg": link,
   "fa-list.svg": list,
   "fa-lock.svg": lock,
+  "fa-map-marker.svg": mapMarker,
   "fa-plus.svg": plus,
   "fa-trash.svg": trash,
   "fa-undo-solid.svg": undo,
