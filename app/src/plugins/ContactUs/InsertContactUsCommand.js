3242289bf5bf8e72de44a1db05cc99d12f67e557
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-10-13 19:56:46 -0700

    GNG-492: Adjust ContactUs plugin to insert block at end of content rather than cursor position

--- app/src/plugins/ContactUs/InsertContactUsCommand.js
+++ app/src/plugins/ContactUs/InsertContactUsCommand.js
@@ -1,15 +1,19 @@
 // ContactUs/InsertContactUsCommand.js
 
 import Command from "@ckeditor/ckeditor5-core/src/command";
-
 export default class InsertContactUsCommand extends Command {
   execute(id) {
+    const root = this.editor.model.document.getRoot();
     this.editor.model.change((writer) => {
       // Insert <contactUs id="...">*</contactUs> at the current selection position
       // in a way which will result in creating a valid model structure.
-      this.editor.model.insertContent(
-        writer.createElement("contactUs", { id })
-      );
+      // this.editor.model.insertContent(
+      //   writer.createElement("contactUs", { id })
+      // );
+
+      // Insert <contactUs id="...">*</contactUs> at the end of the document
+      const contactUs = writer.createElement("contactUs", { id });
+      writer.append(contactUs, root);
     });
   }
 
