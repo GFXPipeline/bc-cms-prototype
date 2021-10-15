6acc5b9188e1afbc79fa808cbf0a64a79ba421d8
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-10-15 15:57:40 -0700

    GNG-492: Add DropdownPanel component to Components/FilterMenu

--- app/src/pages/Components/ComponentsList/FilterMenu/index.js
+++ app/src/pages/Components/ComponentsList/FilterMenu/index.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
+import DropdownPanel from "../../../../components/DropdownPanel";
 import Icon from "../../../../components/Icon";
 
 const StyledDiv = styled.div`
   display: flex;
   flex-direction: row;
-  margin-bottom: 8px;
+  margin: 8px;
 
   div.radio-group {
     align-items: center;
@@ -42,24 +43,64 @@ const StyledDiv = styled.div`
 
     label {
       font-size: 16px;
-      font-weight: 700;
     }
   }
 
-  button {
-    background: none;
-    border: none;
-    cursor: pointer;
-    height: 44px;
+  div.dropdown {
     margin-left: auto;
-    width: 44px;
 
-    &:hover {
-      background-color: #d6d6d6;
+    button {
+      background: none;
+      border: none;
+      cursor: pointer;
+      height: 44px;
+      margin-left: auto;
+      width: 44px;
+
+      &:hover {
+        background-color: #d6d6d6;
+      }
+
+      svg {
+        width: 20px;
+      }
     }
 
-    svg {
-      width: 20px;
+    div.open {
+      padding: 16px;
+      width: 200px;
+
+      fieldset {
+        border: none;
+        padding: 0;
+
+        &:first-child {
+          margin-bottom: 16px;
+        }
+
+        legend {
+          font-size: 16px;
+          font-weight: 700;
+          margin-bottom: 8px;
+        }
+
+        div.checkbox-option {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+
+          input[type="checkbox"] {
+            cursor: pointer;
+            margin: 0;
+          }
+
+          label {
+            cursor: pointer;
+            font-size: 16px;
+            padding-left: 8px;
+          }
+        }
+      }
     }
   }
 `;
@@ -97,9 +138,58 @@ function FilterMenu({ isShowAll, setIsShowAll }) {
         />
         <label htmlFor="my-components">My Components</label>
       </div>
-      <button>
-        <Icon id="fa-filter.svg" />
-      </button>
+      <DropdownPanel
+        className={"dropdown"}
+        buttonAriaLabel={"Filters"}
+        buttonContent={<Icon id="fa-filter.svg" />}
+      >
+        <fieldset>
+          <legend>Filter by status:</legend>
+          <div className="checkbox-option">
+            <input type="checkbox" name="status" id="published" />
+            <label htmlFor="published">Published</label>
+          </div>
+          <div className="checkbox-option">
+            <input type="checkbox" name="status" id="unpublished" />
+            <label htmlFor="unpublished">Unpublished</label>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>Filter by type:</legend>
+          <div className="checkbox-option">
+            <input type="checkbox" name="type" id="accordion" />
+            <label htmlFor="accordion">Accordion</label>
+          </div>
+          <div className="checkbox-option">
+            <input type="checkbox" name="type" id="alert" />
+            <label htmlFor="alert">Alert</label>
+          </div>
+          <div className="checkbox-option">
+            <input type="checkbox" name="type" id="callout" />
+            <label htmlFor="callout">Callout</label>
+          </div>
+          <div className="checkbox-option">
+            <input type="checkbox" name="type" id="contact-us" />
+            <label htmlFor="contact-us">Contact Us</label>
+          </div>
+          <div className="checkbox-option">
+            <input type="checkbox" name="type" id="promo-box" />
+            <label htmlFor="promo-box">Promo Box</label>
+          </div>
+          <div className="checkbox-option">
+            <input type="checkbox" name="type" id="related-links" />
+            <label htmlFor="related-links">Related Links</label>
+          </div>
+          <div className="checkbox-option">
+            <input type="checkbox" name="type" id="table" />
+            <label htmlFor="table">Table</label>
+          </div>
+          <div className="checkbox-option">
+            <input type="checkbox" name="type" id="text" />
+            <label htmlFor="text">Text</label>
+          </div>
+        </fieldset>
+      </DropdownPanel>
     </StyledDiv>
   );
 }
