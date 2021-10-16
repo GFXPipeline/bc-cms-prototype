d9b515ad66014bfc0e8da91737ab34a6b5baae93
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-10-16 16:08:19 -0700

    GNG-492: Fix seed files to delete dependent table data when necessary

--- db/seeds/04_component_types.js
+++ db/seeds/04_component_types.js
@@ -1,17 +1,21 @@
 exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex("component_types")
+  // Delete entries in dependent table `components` first
+  return knex("components")
     .del()
     .then(function () {
-      // Inserts seed entries
-      return knex("component_types").insert([
-        {
-          id: "d632b0f5-99b8-4a73-a1ac-02f6117388db",
-          name: "contact-information",
-          display_name: "Contact information",
-          description:
-            "Add structured contact information that can be re-used across many pages",
-        },
-      ]);
+      return knex("component_types")
+        .del()
+        .then(function () {
+          // Inserts seed entries
+          return knex("component_types").insert([
+            {
+              id: "d632b0f5-99b8-4a73-a1ac-02f6117388db",
+              name: "contact-information",
+              display_name: "Contact information",
+              description:
+                "Add structured contact information that can be re-used across many pages",
+            },
+          ]);
+        });
     });
 };
