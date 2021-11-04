83a836300e8d0aa8eb2a891842a0ea3fc1030d54
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-11-03 17:03:02 -0700

    GNG-553: Add pageService.getPageTypes() front-end API call function

--- app/src/_services/page.service.js
+++ app/src/_services/page.service.js
@@ -180,6 +180,24 @@ async function getPageList() {
   }
 }
 
+// GET request to /api/page-types/
+async function getPageTypes() {
+  try {
+    const headers = authHeader();
+
+    const response = await axios({
+      method: "GET",
+      url: "/api/page-types/",
+      headers,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log("Error in page.service getPageTypes: ", error);
+    throw error;
+  }
+}
+
 export const pageService = {
   create,
   clone,
@@ -187,4 +205,5 @@ export const pageService = {
   update,
   markForDeletion,
   getPageList,
+  getPageTypes,
 };
