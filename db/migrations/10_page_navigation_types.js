1b0732c37e851229bb73e640e8b4e7baa3efea52
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-11-09 18:27:35 -0800

    GNG-553: Add page_navigation_types database table and seed data

---
+++ db/migrations/10_page_navigation_types.js
@@ -0,0 +1,27 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("page_navigation_types", (table) => {
+    table
+      .uuid("id")
+      .defaultTo(knex.raw(`gen_random_uuid()`)) // Postgres built-in UUID v4 generator
+      .notNullable()
+      .primary();
+    table.string("name").unique().notNullable();
+    table.string("display_name").notNullable();
+    table.integer("display_order").notNullable();
+    table.string("description");
+    table.string("icon");
+    table.json("structure");
+    table.uuid("created_by_user").references("id").inTable("users");
+    table.uuid("owned_by_user").references("id").inTable("users");
+    table.uuid("last_modified_by_user").references("id").inTable("users");
+    table.boolean("is_marked_for_deletion").defaultTo(false);
+    table.uuid("marked_for_deletion_by_user").references("id").inTable("users");
+    table.dateTime("time_created").notNullable().defaultTo(knex.fn.now());
+    table.dateTime("time_last_updated").notNullable().defaultTo(knex.fn.now());
+    table.dateTime("time_marked_for_deletion");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("page_navigation_types");
+};
