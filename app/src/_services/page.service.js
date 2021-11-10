b4a909b35b35fe8668d0aac7f8805b13cb9a8914
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-11-09 21:04:18 -0800

    GNG-553: Front-end pageService.create() passes review frequency to API

--- app/src/_services/page.service.js
+++ app/src/_services/page.service.js
@@ -10,6 +10,7 @@ async function create({
   navTitle,
   numberOfCopies,
   pageType,
+  reviewFrequency,
   template,
   title,
 }) {
@@ -30,6 +31,7 @@ async function create({
       navTitle: navTitle || "",
       data: data || "",
       isOnThisPage: isOnThisPage || false,
+      reviewFrequencyMonths: reviewFrequency,
     };
 
     const response = await axios({
