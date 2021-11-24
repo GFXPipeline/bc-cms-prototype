a0b4347255ab065737e38123df065acc50f830f1
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-11-24 13:32:54 -0800

    GNG-567: Add database migration for page_restorations table

---
+++ db/migrations/11_page_restorations.js
@@ -0,0 +1,23 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("page_restorations", (table) => {
+    table
+      .uuid("id")
+      .defaultTo(knex.raw(`gen_random_uuid()`)) // Postgres built-in UUID v4 generator
+      .notNullable()
+      .primary();
+    table
+      .uuid("page_id")
+      .unique()
+      .notNullable()
+      .references("id")
+      .inTable("pages");
+    table.text("reason");
+    table.uuid("created_by_user").references("id").inTable("users");
+    table.dateTime("time_created").notNullable().defaultTo(knex.fn.now());
+    table.dateTime("time_last_updated").notNullable().defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("page_restorations");
+};
