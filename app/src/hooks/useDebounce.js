01e8bb3fd628a35be8729ef2a515828bd3e808ce
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-12-13 10:03:35 -0800

    GNG-570: Add debounce hook for delaying value assignments

---
+++ app/src/hooks/useDebounce.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+// Delay updating a value, such as when a user is typing into a text field
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
