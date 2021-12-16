778b5814e2c277ace9b0056c2b9b47215adffe69
Tyler Krys <Tyler.Krys@gov.bc.ca>
2021-12-16 13:21:24 -0800

    GNG-570: Add CKEditor5 instance for intro field in CreateComponent modal

--- app/src/pages/Components/_actions/CreateComponent/index.js
+++ app/src/pages/Components/_actions/CreateComponent/index.js
@@ -1,10 +1,20 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 
+// Global components & services
 import Modal from "../../../../components/Modal";
 import TextInput from "../../../../components/TextInput";
 import { componentService } from "../../../../_services/component.service";
 import { useDebounce } from "../../../../hooks/useDebounce";
+
+// CKEditor components
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import CKEditorInspector from "@ckeditor/ckeditor5-inspector";
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
+import Link from "@ckeditor/ckeditor5-link/src/link";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
 
 const StyledModal = styled(Modal)``;
 
@@ -25,6 +35,7 @@ const Result = styled.div`
 function CreateComponent({ isOpen, setIsOpen, onAfterClose }) {
   const [name, setName] = useState("");
   const [title, setTitle] = useState("");
+  const [intro, setIntro] = useState("");
   const [search, setSearch] = useState("");
   const debouncedSearch = useDebounce(search, 300);
   const [results, setResults] = useState([]);
@@ -66,6 +77,39 @@ function CreateComponent({ isOpen, setIsOpen, onAfterClose }) {
           onChange={(e) => setTitle(e.target.value)}
         />
       </div>
+      <div className="component-field">
+        <label htmlFor="editor-contact-us">Intro</label>
+      </div>
+      <CKEditor
+        id="editor-contact-us"
+        editor={ClassicEditor}
+        config={{
+          plugins: [Bold, Italic, Link, Paragraph],
+          toolbar: {
+            items: ["bold", "italic", "link"],
+          },
+          language: "en",
+        }}
+        data={intro}
+        onReady={(editor) => {
+          console.log("Component editor ready.", editor);
+
+          // if (process.env.NODE_ENV === "development") {
+          //   CKEditorInspector.attach(editor);
+          // }
+        }}
+        onChange={(event, editor) => {
+          const intro = editor.getData();
+          console.log({ event, editor, intro });
+          setIntro(intro);
+        }}
+        onBlur={(event, editor) => {
+          console.log("Blur.", editor);
+        }}
+        onFocus={(event, editor) => {
+          console.log("Focus.", editor);
+        }}
+      />
       <TextInput value={search} onChange={(e) => setSearch(e.target.value)} />
       {results &&
         Array.isArray(results) &&
