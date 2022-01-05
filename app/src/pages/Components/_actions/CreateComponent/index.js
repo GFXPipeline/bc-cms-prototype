f922e581479ac4b2e536d56ddd0cdbb8c4d8f1b6
Tyler Krys <Tyler.Krys@gov.bc.ca>
2022-01-05 08:27:59 -0800

    GNG-570: Add styling to ContactInformation/AddNew modal

--- app/src/pages/Components/_actions/CreateComponent/index.js
+++ app/src/pages/Components/_actions/CreateComponent/index.js
@@ -2,12 +2,21 @@ import styled from "styled-components";
 
 // Global components & services
 import Modal from "../../../../components/Modal";
-import TextInput from "../../../../components/TextInput";
 
 // Component Types
 import ContactInformation from "./ContactInformation";
 
-const StyledModal = styled(Modal)``;
+const StyledModal = styled(Modal)`
+  .Modal {
+    display: flex;
+    flex-direction: column;
+    height: auto;
+    max-height: 90%;
+    min-height: 80%;
+    max-width: 90%;
+    width: auto;
+  }
+`;
 
 function CreateComponent({ isOpen, setIsOpen, onAfterClose }) {
   return (
